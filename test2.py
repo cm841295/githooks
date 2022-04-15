f29d5b89a19b56e817bb46f57236efa23fcca744
cm841295 <majorchandrakanth8@gmail.com>
2022-04-15 09:10:53 +0000

    hi

--- test2.py
+++ test2.py
@@ -12,14 +12,14 @@ flag = False
 if num > 1:
     # check for factors
     for i in range(2, num):
-        if (num % i) == 0:
+            if (num % i) == 0:
             # if factor is found, set flag to True
             flag = True
             # break out of loop
             break
 
 # check if flag is True
-if flag:
-    print(num, "is not a prime number")
+if flag:            
+print(num, "is not a prime number")
 else:
-    print(num, "is a prime number")
+            print(num, "is a prime number")
